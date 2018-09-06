7caca5bd113855b5dcebeff6b5ebc7885495482c
Christopher Linton <cjlinton83@gmail.com>
2018-09-05 21:13:09 -0500

    ..

--- c/P3.c
+++ c/P3.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_LEN 1024
+#define MAX_ARGS 64
+#define DELIM " \t\n"
 
-char* get_input(char* line) {
-    printf("%s ", "=>");
-    return fgets(line, MAX_LEN, stdin);
+int get_line(char* line) {
+    printf("=> ");
+    if (fgets(line, MAX_LEN, stdin) == NULL)
+        return 0;
+    return strlen(line);
+}
+
+int parse_line(char* line, char** args) {
+    int i = 0;
+
+    if ((args[i++] = strtok(line, DELIM)) == NULL)
+        return 0;    
+    while ((args[i++] = strtok(NULL, DELIM)));
+
+    return i-1;
+}
+
+void run_command(char** args) {
+    for (int i = 0; args[i] != NULL; i++)
+        printf("%s\n", args[i]);
 }
 
 int main(void) {
     char line[MAX_LEN];
-
-    while (get_input(line) != NULL){
-    }
+    char* args[MAX_ARGS];
     
+    while (get_line(line) != 0)
+        if (parse_line(line, args) > 0)
+            run_command(args);
+
     return 0;
 }
