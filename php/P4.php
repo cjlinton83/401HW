a1ff4dc194ab9adfc0b92cadede0854eca2c640a
Christopher Linton <cjlinton83@gmail.com>
2018-10-02 22:18:46 -0500

    ..

--- php/P4.php
+++ php/P4.php
@@ -6,9 +6,11 @@
             $firstName = $_POST['firstName'];
             $lastName = $_POST['lastName'];
             $score = getScore();
+            $grade = getGrade($score);
             
-            print "User: ".$firstName." ".$lastName."\n";
-            print "Score: ".$score."\n";
+            print "User: ".$firstName." ".$lastName."<br>";
+            print "Score: ".$score."<br>";
+            print "Grade: ".$grade."<br>";
 
             function getScore() {
                 $score = 0;
@@ -36,6 +38,23 @@
                 
                 $score *= 10;
                 return $score;
+            }
+
+            function getGrade($score) {
+                $grade = '';
+
+                if (($score <= 100) && (score >= 90))
+                    $grade = 'A';
+                elseif (($score < 90) && (score >= 80))
+                    $grade = 'B';
+                elseif (($score < 80) && (score >= 70))
+                    $grade = 'C';
+                elseif (($score < 70) && (score >= 60))
+                    $grade = 'D';
+                else
+                    $grade = 'F';
+
+                return $grade;
             }
         ?>
     </body>
