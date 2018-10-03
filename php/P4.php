bad17bdd38ab34176677503b73b3263ab5aefe43
Christopher Linton <cjlinton83@gmail.com>
2018-10-03 00:38:48 -0500

    ..

--- php/P4.php
+++ php/P4.php
@@ -8,9 +8,31 @@
             $score = getScore();
             $grade = getGrade($score);
             
+            // Display results from P4.html
             print "User: ".$firstName." ".$lastName."<br>";
             print "Score: ".$score."<br>";
             print "Grade: ".$grade."<br>";
+
+            // Connect to MySQL database
+            $connection = new mysqli('localhost', 'cmps401', 'Mycmps401db', 'cmps401');
+            if ($connection->connect_error)
+                die("<br>Connection failed: ".$connection->connect_error);
+            else
+                print "<br>Connection successful: database cmps401 connected";
+            
+            // Create table to store results from P4.html
+            $query = 'CREATE TABLE bikes (
+                        FirstName varchar(20),
+                        LastName  varchar(20),
+                        Score     int,
+                        Grade     varchar(1),
+                        Date      varchar(20),
+                        PRIMARY KEY (LastName)
+                      )';
+            sendQuery($connection, $query);
+
+            // Close connection to database
+            mysqli_close($connection);
 
             function getScore() {
                 $score = 0;
@@ -56,6 +78,14 @@
                 }
 
                 return $grade;
+            }
+
+            function sendQuery($conn, $q) {
+                if ($r = mysqli_query($conn, $q))
+                    print "<br>Successful Query: ".$q;
+                else
+                    print "<br>Failed Query: ".$q;
+                return $r;
             }
         ?>
     </body>
