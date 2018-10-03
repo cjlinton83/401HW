321e4a19b80dc22dd414cfe9f372c827402f9d09
Christopher Linton <cjlinton83@gmail.com>
2018-10-02 21:59:42 -0500

    ..

--- php/P4.php
+++ php/P4.php
@@ -3,9 +3,38 @@
     </head>
     <body>
         <?php
-            print "Welcome ".$_POST['firstName']."<br>";
-            foreach ($_POST as $e => $f)
-                print("POST[".$e."] = ".$f);
+            $score = getScore();
+
+            print "User: ".$_POST['firstName']." ".$_POST['lastName']."\n";
+            print "Score: ".score."\n";
+
+            function getScore() {
+                $score = 0;
+
+                if ($_POST['q1'] == 'a')
+                    $score++;
+                if ($_POST['q2'] == 'c')
+                    $score++;
+                if ($_POST['q3'] == 'a')
+                    $score++;
+                if ($_POST['q4'] == 'a')
+                    $score++;
+                if ($_POST['q5'] == 'c')
+                    $score++;
+                if ($_POST['q6'] == 'b')
+                    $score++;
+                if ($_POST['q7'] == 'a')
+                    $score++;
+                if ($_POST['q8'] == 'c')
+                    $score++;
+                if ($_POST['q9'] == 'a')
+                    $score++;
+                if ($_POST['q10'] == 'P4.php')
+                    $score++;
+                
+                $score *= 10;
+                return $score;
+            }
         ?>
     </body>
 </html>
