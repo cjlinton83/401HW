5632fcc0f609fbd6d934245a0280950894e1d8d4
Christopher Linton <cjlinton83@gmail.com>
2018-10-02 21:46:55 -0500

    ..

--- php/P4.php
+++ php/P4.php
@@ -4,6 +4,8 @@
     <body>
         <?php
             print "Welcome ".$_POST['firstName']."<br>";
+            foreach ($_POST as $e => $f)
+                print("POST[".$e."] = ".$f);
         ?>
     </body>
 </html>
